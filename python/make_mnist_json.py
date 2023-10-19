f351d54f1e12c0732bfec2228809d640df5c210b
felixp8 <felp8484@gmail.com>
2023-10-19 09:50:39 +0200

    finished(?) game

--- python/make_mnist_json.py
+++ python/make_mnist_json.py
@@ -4,22 +4,22 @@ import json
 import os
 
 digit_dict = {}
-for digit_dir in os.listdir('../data/mnist_png/'):
-    if not os.path.isdir(f'../data/mnist_png/{digit_dir}/'):
+for digit_dir in os.listdir('../images/mnist_png/'):
+    if not os.path.isdir(f'../images/mnist_png/{digit_dir}/'):
         continue
     print(digit_dir)
     digit_dict[digit_dir] = []
-    for img_path in os.listdir(f'../data/mnist_png/{digit_dir}/'):
+    for img_path in os.listdir(f'../images/mnist_png/{digit_dir}/'):
         img_num = int(img_path.strip('.png'))
         digit_dict[digit_dir].append(img_num)
-        img = Image.open(f'../data/mnist_png/{digit_dir}/{img_path}')
+        img = Image.open(f'../images/mnist_png/{digit_dir}/{img_path}')
         pixels = np.array(img).flatten().tolist()
         img.close()
-        with open(f'../data/mnist_json/{img_num}.json', 'w') as f:
-            json.dump(pixels, f)
+        with open(f'../js/mnist/{img_num}.js', 'w') as f:
+            f.write(f'var pixel_data = {pixels};')
     print(len(digit_dict[digit_dir]))
-with open(f'../data/file_map.json', 'w') as f:
-    json.dump(digit_dict, f)
+with open(f'../js/file_map.js', 'w') as f:
+    f.write(f'const map = {json.dumps(digit_dict)};');
     
         
         
--- python/make_pixel_png.py
+++ python/make_pixel_png.py
@@ -2,5 +2,5 @@ from PIL import Image
 
 for num in range(256):
     im = Image.new(mode="L", size=(10,10), color=num)
-    im.save(f'../data/pixels/{num:03d}.png')
+    im.save(f'../images/pixels/{num}.png')
     im.close()
